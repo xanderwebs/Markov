50d1e0e79eed8ae380bbb678b680de8a26bbad3e
xanderwebs <xanderwebs@gmail.com>
2014-05-27 23:38:28 -0400

    Setting up json responses

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -1,8 +1,13 @@
 class RecommendationsController < ApplicationController
+
   def index
     @nodes = Node.all
   end
 
   def show
+    @id = params[:id]
+    respond_to do |format|
+      format.json
+    end
   end
 end
